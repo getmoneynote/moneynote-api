761d8958484ed473ffd99d0df4956131c9507310
markliu2013 <markliu2013@gmail.com>
2023-08-08 22:48:44 +0800

    save

--- moneynote-api-user/src/main/java/cn/biq/mn/user/book/BookService.java
+++ moneynote-api-user/src/main/java/cn/biq/mn/user/book/BookService.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import cn.biq.mn.base.exception.FailureMessageException;
