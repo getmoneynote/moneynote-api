4526dbc29a6cfe6562b2a1412a2e2018d172ea2a
markliu2013 <markliu2013@gmail.com>
2023-08-10 11:32:38 +0800

    save

--- moneynote-api-user/src/main/java/cn/biq/mn/user/TestController.java
+++ moneynote-api-user/src/main/java/cn/biq/mn/user/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/version")
     public BaseResponse handleVersion() {
-        return new DataResponse<>(83);
+        return new DataResponse<>(84);
     }
 
     @GetMapping("/test3")
