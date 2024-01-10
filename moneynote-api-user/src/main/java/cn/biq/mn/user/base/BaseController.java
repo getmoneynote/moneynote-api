92e75134e10ff9b1941b5a25d03ad05012a61c94
markliu2013 <markliu2013@gmail.com>
2024-01-10 18:06:40 +0800

    save

---
+++ moneynote-api-user/src/main/java/cn/biq/mn/user/base/BaseController.java
@@ -0,0 +1,8 @@
+package cn.biq.mn.user.base;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+//@CrossOrigin(origins = "*")
+public class BaseController {
+
+}
--- moneynote-api-user/src/main/java/cn/biq/mn/user/interceptor/MvcInterceptorConfig.java
+++ moneynote-api-user/src/main/java/cn/biq/mn/user/interceptor/MvcInterceptorConfig.java
@@ -2,6 +2,7 @@ package cn.biq.mn.user.interceptor;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,6 +22,23 @@ public class MvcInterceptorConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/version")
                 .excludePathPatterns("/test*");
         WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(false)
+                //设置放行哪些原始域   SpringBoot2.4.4下低版本使用.allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                //放行哪些请求方式
+//                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                .allowedMethods("*") //或者放行全部
+                //放行哪些原始请求头部信息
+                .allowedHeaders("*")
+                //暴露哪些原始请求头部信息
+                .exposedHeaders("*");
     }
 
 }
