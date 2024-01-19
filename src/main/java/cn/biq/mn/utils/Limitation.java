b2144c75aa364555988bbe3c4fb7db0724425a60
markliu2013 <markliu2013@gmail.com>
2024-01-19 22:54:50 +0800

    save

--- src/main/java/cn/biq/mn/utils/Limitation.java
+++ src/main/java/cn/biq/mn/utils/Limitation.java
@@ -3,12 +3,12 @@ package cn.biq.mn.utils;
 public final class Limitation {
 
     public final static Integer category_max_level = 4;
-    public final static Integer group_max_count = 500;
-    public final static Integer book_max_count = 20000;
-    public final static Integer account_max_count = 30000;
-    public final static Integer category_max_count = 90000;
-    public final static Integer tag_max_count = 90000;
-    public final static Integer payee_max_count = 90000;
+    public final static Integer group_max_count = 5000;
+    public final static Integer book_max_count = 200000;
+    public final static Integer account_max_count = 3000000;
+    public final static Integer category_max_count = 90000000;
+    public final static Integer tag_max_count = 90000000;
+    public final static Integer payee_max_count = 90000000;
     public final static Integer flow_max_count_daily = 999999999;
     public final static Integer flow_max_category_count = 10;//每个账单最多包含分类个数
     public final static Integer register_ip_max_count = 5;
