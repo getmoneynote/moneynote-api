eecc2ad057634a91cccbd92882960a41d7108a5d
markliu2013 <markliu2013@gmail.com>
2024-03-07 11:45:24 +0800

    save

--- src/main/java/cn/biq/mn/TestController.java
+++ src/main/java/cn/biq/mn/TestController.java
@@ -21,7 +21,7 @@ public class TestController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/version")
     public BaseResponse handleVersion() {
-        return new DataResponse<>("93.32");
+        return new DataResponse<>("93.33");
     }
 
     @GetMapping("/test3")
